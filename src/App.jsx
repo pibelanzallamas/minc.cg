4e385314fb8eafcf9aeeeb3dfb4a15b3cdc50f5a
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-10-24 16:24:42 -0300

    ADD: greeting box

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,15 @@ function App() {
   return (
     <>
       <h1>minc</h1>
+      <section className="greetings">
+        <div className='greetings-title'>Greetings</div>
+        <div className='greetings-box'>
+          <p className='greetings-box-text'>
+            Welcome to <span>minc CGC</span>. Although fully functional from a mobile device, 
+            our facility is best experienced from a desktop.
+          </p>
+        </div>
+      </section>
     </>
   )
 }
