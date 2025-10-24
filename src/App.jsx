b22f37d34fe446846008470e3a32d27b60ae10eb
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-10-24 14:26:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './styles/app.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>minc</h1>
+    </>
+  )
+}
+
+export default App
