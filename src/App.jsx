6181e4e67819f513088450f5686253a7849cbe8c
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-10-25 11:32:22 -0300

    UPDATE: home barebones

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,30 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>minc</h1>
-      <section className="greetings">
-        <div className='greetings-title'>Greetings</div>
-        <div className='greetings-box'>
-          <p className='greetings-box-text'>
-            Welcome to <span>minc CGC</span>. Although fully functional from a mobile device, 
-            our facility is best experienced from a desktop.
-          </p>
-        </div>
-      </section>
-    </>
+    <div className='wraper'>
+      <nav className='navbar'>
+        <h1 className='navbar-logo'>CGC</h1>
+        <ul className='navbar-menu'>
+          <li>Menu</li>
+          <li>Videos</li>
+          <li>Market</li>
+        </ul>
+      </nav>
+      <main>
+        <section className="greetings">
+          <div className='greetings-title'>
+            <div className='greetings-title-number'>00</div>
+            <div className='greetings-title-salute'>GREETINGS</div>
+          </div>
+          <div className='greetings-box'>
+            <p className='greetings-box-text'>
+              Welcome to <span>minc CGC</span>. Although fully functional from a mobile device, 
+              our facility is best experienced from a desktop.
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
 
