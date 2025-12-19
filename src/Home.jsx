12628e7e670c3b54aafc4b743a2b904448fbbd78
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-12-19 14:52:16 -0300

    update: re scale home mobile

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,21 +6,16 @@ import bar from "./assets/home/whiteBar@300x.png"
 
 function Home() {
     return(
-    <main>
-      <div className="home-mobile">
-          <div className="greetings-section">
-            <figure className="greetings-message">
-              <figure>
-                <img src={oo}></img>
-              </figure>
-              <img src={greetings}></img>
-            </figure>
-          </div>
+      <main>
+        <div className="home-mobile">
+          <figure className="greetings-message">
+            <img src={greetings}></img>
+          </figure>
           <figure className="home-white-bar">
             <img src={bar}></img>
           </figure> 
-      </div>
-    </main>
+        </div>
+      </main>
     )
 }
 
