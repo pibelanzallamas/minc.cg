ff4f6de7a99096d776b393edc7f237d350632abb
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-12-27 10:50:37 -0300

    update market mobile first version

--- src/Market.jsx
+++ src/Market.jsx
@@ -1,15 +1,23 @@
 import ItemCard from './commons/ItemCard.jsx'
-// import marketTitle from "./assets/market/"
+import marketTitle from "./assets/market/marketTitle.png"
 import {market} from "./utils/market.js"
+import descriptionBox from "./assets/market/descriptBox.png"
+import viewCart from "./assets/market/viewCart.png"
 
 function Market(){
   return(
     <main className="content-page">
       <figure className="content-title-text">
-        {/* <img src={marketTitle} alt="market-title" /> */}
+        <img src={marketTitle} alt="market-title" />
       </figure>
-      <section className="card-section">
 
+      <section className="description-section">
+        <figure className="description-box">
+          <img src={descriptionBox} alt="market-title" />
+        </figure>
+        <figure className="view-cart">
+          <img src={viewCart} alt="market-title" />
+        </figure>
       </section>
 
       <section className="market">
