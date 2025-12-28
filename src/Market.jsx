2baac2457c5d1a118cd0cef0f260b1edb0068fb2
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-12-28 11:01:09 -0300

    add get products

--- src/Market.jsx
+++ src/Market.jsx
@@ -1,17 +1,39 @@
+import { useEffect. useState } from 'react'
 import ItemCard from './commons/ItemCard.jsx'
 import marketTitle from "./assets/market/marketTitle.png"
-// import {market} from "./utils/market.js"
 import descriptionBox from "./assets/market/descriptBox.png"
 import viewCart from "./assets/market/viewCart.png"
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 function Market(){
+  const [allProducts, setAllProducts] = useState([]);
+
+
+  useEffect(() => {
+    setAllProducts(getProducts());
+  }, []);
+
+  const getProducts = async () => {
+    try {
+      const res = await axios.get(
+        "https://minc-cg-back.onrender.com/products/"
+      );
+      console.log(res.data);
+      return res.data;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      throw error;
+    }
+  };
+
+
   return(
     <main className="content-page">
       <figure className="content-title-text">
         <img src={marketTitle} alt="market-title" />
       </figure>
-
+      {console.log(allProducts)}
       <section className="description-section">
         <figure className="description-box">
           <img src={descriptionBox} alt="market-title" />
@@ -24,9 +46,9 @@ function Market(){
       </section>
 
       <section className="market">
-        {/* {market.map((e)=>(
+        {allProducts.length > 0 && allProducts.map((e)=>(
           <ItemCard itemData={e} />
-        ))} */}
+        ))}
       </section>
     </main>
   )
