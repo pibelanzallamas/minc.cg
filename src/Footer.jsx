2efbf2ee69314d64ce4c7ed664c32294eb690806
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-12-29 15:26:49 -0300

    about button with hover

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,13 +1,20 @@
-import aboutUs from "./assets/home/aboutBox.png"
+import aboutUs from "./assets/home/aboutUsText.png"
 import menuBox from "./assets/buttons/oneBoxShadow.png"
 import menuBoxSel from "./assets/buttons/menuBoxesShadowSel.png"
+import { Link } from "react-router-dom"
 
 function Footer() {
   return(
     <footer>
-      <figure className="about-us-button">
-        <img src={aboutUs} alt="" />
-      </figure>
+      <div className="about-us">
+        <figure className="about-us-text">
+          <img src={aboutUs}></img>
+        </figure>
+        <Link to="/about-us" className="about-us-link">
+          <figure className="about-us-box"><img src={menuBox} alt="" /></figure>
+          <figure className="about-us-sel-box"><img src={menuBoxSel} alt="" /></figure>
+        </Link>
+      </div>
     </footer>
 
   )
