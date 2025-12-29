09a1261760bd68edce3f8f27fa8673f6df7937f0
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-12-29 10:39:56 -0300

    desktop bar optimized styles

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,70 +8,69 @@ import vButton from "./assets/buttons/VidBttn@300x.png"
 import mButton from "./assets/buttons/markBttn@300x.png"
 import newButton from "./assets/buttons/new@300x.png"
 import menuBar from "./assets/buttons/menuBar.png"
-import box from "./assets/buttons/oneBox.png"
-import boxSel from "./assets/buttons/oneBoxSelected.png"
+import menuBox from "./assets/buttons/oneBoxShadow.png"
+import menuBoxSel from "./assets/buttons/menuBoxesShadowSel.png"
 
 
 function Navbar() {
     const [extraMenu, setExtraMenu] = useState(false);
 
     return(
-        <>
+      <>
         <nav className='navbar'>
           <Link to="/" className='navbar-logotipo' onClick={()=>setExtraMenu(false)}>
               <img src={logo} alt='cgcminic-logo'></img>
           </Link>
           
-          <figure className="menu-desktop-bar">
-            <img className="menu-bar" src={menuBar}></img>
-            {/* <figure className="menu-desktop-box">
-              <Link to={"/programming"} className="box-link">
-                <img className="box" src={box} alt="" />
-                <img className="box-selected" src={boxSel} alt="" />
-              </Link>
-              <Link to={"/videos"}>
-                <img className="box" src={box} alt="" />
-                <img className="box-selected" src={boxSel} alt="" />
-              </Link>
-              <Link to={"/market"}>
-                <img className="box" src={box} alt="" />
-                <img className="box-selected" src={boxSel} alt="" />
-              </Link>
-            </figure> */}
-          </figure>
-          
-          <figure className="navbar-menu-button">
+          {/* MENU DESKTOP */}
+          <div className="menu-desktop">
+            <figure className="menu-desktop-bar">
+              <img src={menuBar}></img>
+            </figure>
+            <Link to="/programming">
+                <figure className="menu-single-box box-1 box-not-sel"><img src={menuBox} alt="" /></figure>
+                <figure className="menu-single-box box-1 box-sel"><img src={menuBoxSel} alt="" /></figure>
+            </Link>
+            <Link to="/videos">
+                <figure className="menu-single-box box-2"><img src={menuBox} alt="" /></figure>
+            </Link>
+            <Link to="/market">
+                <figure className="menu-single-box box-3"><img src={menuBox} alt="" /></figure>
+            </Link>
+          </div>
+         
+         {/* MENU MOBILE */}
+          <figure className="menu-mobile-button">
             <img onClick={()=>setExtraMenu(!extraMenu)} src={menuButton}></img>
             <figure  className={extraMenu ? "navbar-menu-line" : "menu-closed"}>
               <img src={menuLine}></img>
             </figure>  
           </figure>
-
+        
         </nav>
 
+        {/* EXTRA MENU MOBILE */}
         <div className={extraMenu ? "extra-menu" : "menu-closed"}>
-          <ul className="extra-menu-buttons-div">
+          <div className="extra-menu-buttons-div">
             <Link to={"/programming"} onClick={()=>setExtraMenu(!extraMenu)}>
               <figure className="navbar-extra-button">
                 <img src={pButton}></img>
               </figure>
             </Link>
-
             <Link to={"/videos"} onClick={()=>setExtraMenu(!extraMenu)}>
               <figure className="navbar-extra-button">
                 <img src={vButton}></img>
               </figure>
             </Link>
-
-            <Link to={"/market"} onClick={()=>setExtraMenu(!extraMenu)}>
-              <figure className="navbar-extra-button new-button-div">
+            <figure className="navbar-extra-button new-button-div">
+              <Link to={"/market"} onClick={()=>setExtraMenu(!extraMenu)}>
                 <img src={mButton}></img>
-                <figure className="new-button">
-                  <img src={newButton}></img>
-                </figure>
+              </Link>
+              <figure className="new-button">
+                <img src={newButton}></img>
               </figure>
-            </Link>
-          </ul>
+            </figure>
+          </div>
         </div>
 
       </>
@@ -79,3 +78,15 @@ function Navbar() {
 }
 
 export default Navbar;
+
+          {/* no me reconoce el LINK, estoy hasta l
+            
+            tengo que poner los boxes, creo que es mnejor hacerlos de cero
+            
+            asi les podes poner hover
+            
+            me quiero comprar remeras piolas no me acuerdo donde quedaba la tienda
+            
+            AGUANTE LINUX QUIERO AMIGOS SOLO QUE CONOSCAN LINUXXXX
+            
+            VAS BIEN*/}
