0a42245a85584b5321fdffad57cda8a23045e602
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-12-29 17:29:39 -0300

    market desktop frist aprocah

--- src/commons/ItemCard.jsx
+++ src/commons/ItemCard.jsx
@@ -5,6 +5,7 @@ function ItemCard({itemData}){
   return(
     <div className="item-card">
       <figure className="item-image"><img src={itemData.image}/></figure>
+      
       <div className="item-info">
         <p className="item-title">{itemData.title}</p>
         <div className="item-description">
@@ -14,6 +15,7 @@ function ItemCard({itemData}){
           <p className="item-price">€{Math.trunc(itemData.price)}</p>
         </div>
       </div>
+      
       <figure className="add-to-cart"><img src={addToCard} alt="" /></figure>
     </div>
 
