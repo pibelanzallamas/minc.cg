f700501eb16318794ac55dd416416510a9082350
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-12-29 21:37:56 -0300

    update home sass optimized

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,24 +4,24 @@ import leftCharacter from "./assets/home/characterLeft.png"
 import rightCharacter from "./assets/home/characterRight.png"
 
 function Home() {
-    return(
-      <main className="welcome-page">
-        <figure className="home-figures figure-a">
-            <img src={leftCharacter}></img>
+  return(
+    <main className="welcome-page">
+      <figure className="home-figures figure-a">
+          <img src={leftCharacter}></img>
+      </figure>
+      <section className="greetings-section">
+        <figure className="greetings-message">
+          <img src={greetings}></img>
         </figure>
-        <section className="greetings-section">
-          <figure className="greetings-message">
-            <img src={greetings}></img>
-          </figure>
-          <figure className="home-char-bar">
-            <img src={bar}></img>
-          </figure> 
-        </section>
-        <figure className="home-figures figure-b">
-            <img src={rightCharacter}></img>
-        </figure>
-      </main>
-    )
+        <figure className="home-char-bar">
+          <img src={bar}></img>
+        </figure> 
+      </section>
+      <figure className="home-figures figure-b">
+          <img src={rightCharacter}></img>
+      </figure>
+    </main>
+  )
 }
 
 export default Home;
