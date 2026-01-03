1af331e2d752d866122a5bb0a99bb7d02235e527
Brandon Castillo <brandoncastillo.09@gmail.com>
2026-01-03 15:09:06 -0300

    first assets imported for programming

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/videos" element={<Videos />} />
         <Route path="/programming" element={<Programming/>}/>
+        <Route path="/videos" element={<Videos />} />
         <Route path="/market" element={<Market/>}/>
       </Routes>
       <Footer/>
--- src/Programming.jsx
+++ src/Programming.jsx
@@ -1,9 +1,21 @@
+import programTitle from "./assets/programming/programTitle.png"
+import introProgam from "./assets/programming/introProgram.png"
+
 function Programming(){
-return(
-    <h2></h2>
+  return(
+    <main className="content-page">
+      <figure className="content-title-text">
+        <img src={programTitle} alt="video-title" />
+      </figure>
+
+      <section className="description-section">
+        <figure className="description-box">
+          <img src={introProgam} alt="market-title" />
+        </figure>
+      </section>
+
+    </main>
   )
-
-
 }
 
 export default Programming;
