091c2e8c0e0628ec0f9e9d73015d9069adbd9e63
Brandon Castillo <brandoncastillo.09@gmail.com>
2026-01-04 12:38:44 -0300

    programming mobile

--- src/Programming.jsx
+++ src/Programming.jsx
@@ -6,33 +6,22 @@ import flag from "./assets/programming/flag.png"
 import unionBlackTitle from "./assets/programming/UBtitle.png"
 import uBDescription from "./assets/programming/description.png"
 import {programming} from "./utils/programming"
+import { Link } from "react-router-dom"
 
 function Programming(){
   return(
     <main className="content-page">
-      <figure className="content-title-text">
+      <figure className="content-title-text programming-title">
         <img src={programTitle} alt="programming-title" />
       </figure>
 
-      <figure className="description-box">
+      <figure className="programming-description">
         <img src={introProgam} alt="market-title" />
       </figure>
 
-      <figure className="description-box">
+      <figure className="programming-image">
         <img src={globe} alt="globe-icon" />
       </figure>
-{/* 
-      <figure className="description-box">
-        <img src={flag} alt="union-black-flag" />
-      </figure>
-
-      <figure className="description-box">
-        <img src={unionBlackTitle} alt="union-black-title" />
-      </figure>
-
-      <figure className="description-box">
-        <img src={uBDescription} alt="description-black-union" />
-      </figure> */}
 
       <div className="articles-grid">
         {programming.map(e =>(
@@ -40,9 +29,11 @@ function Programming(){
             <figure className="article-image-select">
               <img src={e.image} alt="union-black-flag" />
             </figure> 
+            <Link to="programming-1/">
             <figure className="article-title-select">
               <img src={e.titleSelected} alt="union-black-flag" />
             </figure> 
+            </Link>
             <div className="article-description-select">{e.epilogoSelected.split("\n\n").map((paragraph, index) => 
             (<><p key={index}>{paragraph}</p><br/></>))}</div>
           </div>
