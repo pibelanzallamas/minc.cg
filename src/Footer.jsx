1876493329f53b0ae5923a029044429d6f11b928
Brandon Castillo <brandoncastillo.09@gmail.com>
2026-01-13 11:36:21 -0300

    update about us desktop

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,22 +1,28 @@
 import aboutUs from "./assets/home/aboutUsText.png"
 import menuBox from "./assets/buttons/oneBoxShadow.png"
 import menuBoxSel from "./assets/buttons/menuBoxesShadowSel.png"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 function Footer() {
+  const location = useLocation();
+  const isAboutUsPage = location.pathname === "/about-us";
+  
   return(
-    <footer>
+    <footer className={isAboutUsPage ? "disabled" : ""}>
       <div className="about-us">
         <figure>
-          <img src={aboutUs}></img>
+          <img src={aboutUs} alt="About Us" />
         </figure>
-        <Link to="/about-us" className="about-us-link">
-          <figure className=" about-us-box"><img src={menuBox} alt="" /></figure>
-          <figure className=" about-us-sel-box"><img src={menuBoxSel} alt="" /></figure>
+        <Link to="/about-us" className={`about-us-link ${isAboutUsPage ? "disabled-link" : ""}`}>
+          <figure className="about-us-box">
+            <img src={menuBox} alt="Menu box" />
+          </figure>
+          <figure className="about-us-sel-box">
+            <img src={menuBoxSel} alt="Selected menu box" />
+          </figure>
         </Link>
       </div>
     </footer>
-
   )
 }
 
